ab0fc2a2af6a029b36ac899e9c285de753ba2dd6
1aurend <ldavidson@g.harvard.edu>
2019-12-03 13:59:22 -0500

    Eject and add custom start scripts

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './single-component/App' /*replace this directory with ./example or ./starter to run the other templates*/
-
-ReactDOM.render(<App />, document.getElementById('root'));
