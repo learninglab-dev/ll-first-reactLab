2578a027f92fda2d3fa4e9ba5e2e81d4a75a5bae
1aurend <ldavidson@g.harvard.edu>
2019-12-04 13:55:04 -0500

    Swap 2nd gif

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './example/App' /*replace this directory with ./single-component or ./starter to run the other templates*/
+import App from './mock-fetch-example/App' /*replace this directory with ./single-component or ./starter to run the other templates*/
 
 ReactDOM.render(<App />, document.getElementById('root'));
