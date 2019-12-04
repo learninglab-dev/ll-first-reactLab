f8018670dc7c61d51e61709c5f5b3eb7b997c42c
1aurend <ldavidson@g.harvard.edu>
2019-12-04 13:50:24 -0500

    Improve grid layout and change 2nd gif

--- src/mock-fetch-example/data.js
+++ src/mock-fetch-example/data.js
@@ -1,7 +1,7 @@
 const urls = [
   {
     img: 'https://images.pexels.com/photos/416160/pexels-photo-416160.jpeg',
-    gif: 'https://media.giphy.com/media/HZUHce5TyYixq/giphy.gif',
+    gif: 'https://media.giphy.com/media/VS3f7UiKom7hS/giphy.gif',
   },
   {
     img: 'https://images.pexels.com/photos/127027/pexels-photo-127027.jpeg',
--- src/starter/Button.js
+++ src/starter/Button.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 //a basic button with even more basic styling
 function Button({ onClick, children }) {
-  return <button onClick={onClick} style={{border: '5px solid white', padding: 0}}> {children} </button>
+  return <button onClick={onClick} style={{border: '5px solid white', padding: 0, marginBottom: '100px'}}> {children} </button>
 }
 
 export default Button
