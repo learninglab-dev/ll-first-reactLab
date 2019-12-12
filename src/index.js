caadee71433b8f42e04754c1a9fd70c03e185c9d
1aurend <ldavidson@g.harvard.edu>
2019-12-12 09:45:06 -0500

    Add live-editor app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './mock-fetch-example/App' /*replace this directory with ./single-component or ./starter to run the other templates*/
+// import App from './mock-fetch-example/App' /*replace this directory with ./single-component or ./starter to run the other templates*/
+import SaveContext from './live-editor/SaveContext'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<SaveContext />, document.getElementById('root'));
