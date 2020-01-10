34ae255cfa6e970d32caf18576224df978bf7e0c
Lauren Davidson <ldavidson@g.harvard.edu>
2020-01-10 11:11:58 -0500

    Reset App.js

--- src/02_starter/App.js
+++ src/02_starter/App.js
@@ -2,11 +2,11 @@ import React from 'react'
 import Layout from './Layout'
 import imgSources from './data'
 
+
 //the top-level component for our app. I've added some basic styling and a flexbox layout for you so your gifs make a nice grid :)
 export default function App() {
   return (
     <Layout>
-      <h2>hello world!</h2>
     </Layout>
   )
 }
