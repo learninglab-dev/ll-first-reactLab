410f757cba6934f466006e53e917e4655ae90416
Lauren Davidson <ldavidson@g.harvard.edu>
2020-02-12 08:57:01 -0500

    Where we left off

--- src/02_starter/App.js
+++ src/02_starter/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Layout from './Layout'
 import imgSources from './data'
+import Image from './Img'
 
 
 //the top-level component for our app. I've added some basic styling and a flexbox layout for you so your gifs make a nice grid :)
 export default function App() {
   return (
     <Layout>
+      <Image hi='https://images.pexels.com/photos/416160/pexels-photo-416160.jpeg' test='test' another='another' />
+      <Image hi='https://images.pexels.com/photos/127027/pexels-photo-127027.jpeg' />
+      <Image hi='https://images.pexels.com/photos/1564506/pexels-photo-1564506.jpeg' />
     </Layout>
   )
 }
--- src/02_starter/Img.js
+++ src/02_starter/Img.js
@@ -1,1 +1,15 @@
 //a component that displays an image goes here
+import React from 'react'
+
+export default function Image(props) {
+
+  props = {
+    hi: 'url goes here',
+    test: 'test',
+    another: 'another'
+  }
+
+
+  const someVariable = props.hi
+  return <img src={someVariable} alt='cute kitten' width='300px'/>
+}
