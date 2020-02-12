ed50d3f439ab31618b7deecdbf27b11e00a9969d
Lauren Davidson <ldavidson@g.harvard.edu>
2020-02-12 09:08:39 -0500

    Cleanup

--- src/02_starter/App.js
+++ src/02_starter/App.js
@@ -2,15 +2,16 @@ import React from 'react'
 import Layout from './Layout'
 import imgSources from './data'
 import Image from './Img'
+import { Row, Col } from 'shards-react'
 
 
 //the top-level component for our app. I've added some basic styling and a flexbox layout for you so your gifs make a nice grid :)
 export default function App() {
   return (
     <Layout>
-      <Image hi='https://images.pexels.com/photos/416160/pexels-photo-416160.jpeg' test='test' another='another' />
-      <Image hi='https://images.pexels.com/photos/127027/pexels-photo-127027.jpeg' />
-      <Image hi='https://images.pexels.com/photos/1564506/pexels-photo-1564506.jpeg' />
+      <Image url='https://images.pexels.com/photos/416160/pexels-photo-416160.jpeg' test='test' another='another' />
+      <Image url='https://images.pexels.com/photos/127027/pexels-photo-127027.jpeg' />
+      <Image url='https://images.pexels.com/photos/1564506/pexels-photo-1564506.jpeg' />
     </Layout>
   )
 }
--- src/02_starter/Img.js
+++ src/02_starter/Img.js
@@ -3,13 +3,11 @@ import React from 'react'
 
 export default function Image(props) {
 
-  props = {
-    hi: 'url goes here',
-    test: 'test',
-    another: 'another'
-  }
+  // props = {
+  //   url: 'url goes here',
+  //   test: 'test',
+  //   another: 'another'
+  // }
 
-
-  const someVariable = props.hi
-  return <img src={someVariable} alt='cute kitten' width='300px'/>
+  return <img src={props.url} alt='cute kitten' width='300px'/>
 }
