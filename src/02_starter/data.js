29677cccf40cdbaa4755cd06d178afa99e32f1da
Lauren Davidson <ldavidson@g.harvard.edu>
2020-02-19 11:00:41 -0500

    Add map over data

--- src/02_starter/data.js
+++ src/02_starter/data.js
@@ -1,8 +1,16 @@
 //the data structure we'll use to build our app
 const urls = [
   {
-    img: '',
-    gif: '',
+    img: 'https://images.pexels.com/photos/416160/pexels-photo-416160.jpeg',
+    gif: 'https://media.giphy.com/media/HZUHce5TyYixq/giphy.gif',
+  },
+  {
+    img: 'https://images.pexels.com/photos/127027/pexels-photo-127027.jpeg',
+    gif: 'https://media.giphy.com/media/VS3f7UiKom7hS/giphy.gif',
+  },
+  {
+    img: 'https://images.pexels.com/photos/1564506/pexels-photo-1564506.jpeg',
+    gif: 'https://media.giphy.com/media/XHY2TRKxJQfsrwS33v/giphy.gif'
   }
 ]
 
